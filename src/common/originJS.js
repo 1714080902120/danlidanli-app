9ad1c7cebb10daf2664d18df8f3b05c423cc8d4e
1714080902120 <1466645277@qq.com>
2020-06-02 17:18:55 +0800

    2020-06-02 17:18

--- src/common/originJS.js
+++ src/common/originJS.js
@@ -27,3 +27,19 @@ export async function pay (datas) {
   xhr.open('GET', AlipayServer)
   xhr.send()
 }
+
+export async function takePhoto(){
+  var _self=this;
+  var cmr = plus.camera.getCamera();
+  var fmt = cmr.supportedImageFormats[0];
+  cmr.captureImage( function( path ){
+      plus.io.resolveLocalFileSystemURL(path, function(entry) {
+             var tmpPath= entry.toLocalURL();
+             var fileAry=[];
+             fileAry.push(tmpPath);
+             _self.compressImg(fileAry);
+      })
+    },
+    function( error ) {},
+    {format:fmt});
+}
